2cdbd7ed577c1b2ac85260507c4e0de292f35117
denizylmnbs <denizylmnbsss@gmail.com>
2025-03-06 22:28:23 +0200

    First Controller and Swagger

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManagementAPI.Data;
+using TaskManagementAPI.Models;
 
 namespace TaskManagementAPI.Controllers
 {
@@ -7,6 +10,26 @@ namespace TaskManagementAPI.Controllers
     [ApiController]
     public class TaskController : ControllerBase
     {
+        private readonly TaskManagementContext _context;
 
+        public TaskController(TaskManagementContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllTasks()
+        {
+            var tasks = await _context.Tasks.ToListAsync();
+            var response = tasks.Select(response => new TaskDto
+            {
+                Title = response.Title,
+                Description = response.Description,
+                IsCompleted = response.IsCompleted,
+                CreatedAt = response.CreatedAt,
+                Deadline = response.Deadline
+            }).ToList();
+
+            return Ok(response);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using TaskManagementAPI.Data;
 
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<TaskManagementContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
@@ -15,7 +17,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
